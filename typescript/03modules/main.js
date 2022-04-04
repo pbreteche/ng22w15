32b232d896c0522983f2352910a6bbd4c88e5119
Pierre Bretéché <pbreteche@dawan.fr>
2022-04-04 16:31:23 +0200

    module es2020

---
+++ typescript/03modules/main.js
@@ -0,0 +1,3 @@
+import { Contact as Customer } from "./contact";
+const c1 = new Customer();
+c1.name = 'test';
---
+++ typescript/03modules/main.ts
@@ -0,0 +1,4 @@
+import { Contact as Customer } from "./contact";
+
+const c1 = new Customer();
+c1.name = 'test';
