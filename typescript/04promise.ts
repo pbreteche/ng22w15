eaefd0fff9f759e5bdb40c1b51f3604cd59fd4c3
Pierre Bretéché <pbreteche@dawan.fr>
2022-04-07 11:34:18 +0200

    Promesse avec gestion d'échec

--- typescript/04promise.ts
+++ typescript/04promise.ts
@@ -8,11 +8,16 @@ const myPromise = new Promise((resolve, reject) => {
 (function() { // IIFE : Immediatly invocated function expression
     // association de la fonction "resolve"
     myPromise.then(value => console.log(value))
+        .catch(err => console.error(`Erreur : ${err}`))
 })();
 
 
 (async function() { // la fonction async peut être interrompue
     // await permet à la foction de rendre la main, le temps que la promesse soit résolue
-    const value = await myPromise;
-    console.log(value)
+    try {
+        const value = await myPromise;
+        console.log(value)
+    } catch (err) {
+        console.error(`Erreur : ${err}`)
+    }
 })();
